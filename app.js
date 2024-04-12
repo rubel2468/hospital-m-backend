b891e478e42fe1e4a10e0dddfb1965f0ecc1aa7f
Rubel <132301506+rubel2468@users.noreply.github.com>
2024-04-12 20:01:15 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+// import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+
+import  path from 'path';
+import { dirname } from "path/posix";
+
+const app = express();
+// config({ path: "../config/config.env" });
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  console.log(dirname)
+});
+
+app.use(
+  cors({
+    origin:[`$(window.location.orgin)`],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
---
+++ server.js
@@ -0,0 +1,13 @@
+import app from "./app.js";
+import cloudinary from "cloudinary";
+
+
+cloudinary.v2.config({
+  cloud_name:"dvozhxu6n",
+  api_key:"442261259376517" ,
+  api_secret:"Qav6pUTxDhE6IY0Us9DoaY6jBzc" ,
+});
+
+app.listen(4000, () => {
+console.log("server is running" )
+});
